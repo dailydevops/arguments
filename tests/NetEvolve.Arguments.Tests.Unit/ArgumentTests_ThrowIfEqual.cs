ed087c872ac29130620e45ba30fde348d4c73b6e
Martin Stühmer <me@samtrion.net>
2024-04-05 12:16:28 +0200

    chore(tests): Tests for newly introduced methods added

---
+++ tests/NetEvolve.Arguments.Tests.Unit/ArgumentTests_ThrowIfEqual.cs
@@ -0,0 +1,35 @@
+﻿namespace NetEvolve.Arguments.Tests.Unit;
+
+using System;
+using Xunit;
+
+public sealed partial class ArgumentTests
+{
+    [Fact]
+    public void ThrowIfEqual_WhenArgumentIsEqualToMaximum_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        var argument = 1;
+        var maximum = 1;
+
+        // Act
+        void Act() => Argument.ThrowIfEqual(argument, maximum);
+
+        // Assert
+        _ = Assert.Throws<ArgumentOutOfRangeException>("argument", Act);
+    }
+
+    [Fact]
+    public void ThrowIfEqual_WhenArgumentIsNotEqualToMaximum_ReturnsArgument()
+    {
+        // Arrange
+        var argument = 2;
+        var maximum = 1;
+
+        // Act
+        Argument.ThrowIfEqual(argument, maximum);
+
+        // Assert
+        Assert.True(true);
+    }
+}
