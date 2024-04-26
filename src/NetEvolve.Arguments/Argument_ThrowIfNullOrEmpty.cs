bbc1151ebd90cb389202ccc40078388f9d8c3429
Martin Stühmer <me@samtrion.net>
2024-04-26 23:20:45 +0200

    feat: Added `ThrowIfNullOrEmpty(IEnumerable<T>)` (#133)
    * feat: Added `ThrowIfNullOrEmpty(IEnumerable`1)`
    
    * docs: Added XML Summary to ThrowIfNullOrEmpty method

--- src/NetEvolve.Arguments/Argument_ThrowIfNullOrEmpty.cs
+++ src/NetEvolve.Arguments/Argument_ThrowIfNullOrEmpty.cs
@@ -1,8 +1,10 @@
 ﻿namespace NetEvolve.Arguments;
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 public static partial class Argument
@@ -37,5 +39,29 @@ public static partial class Argument
             ThrowArgumentException(paramName);
         }
 #endif
+    }
+
+    /// <summary>Throws an exception if <paramref name="argument"/> is null or empty.</summary>
+    /// <param name="argument">The string argument to validate as non-null and non-empty.</param>
+    /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="argument"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="argument"/> is empty.</exception>
+    [DebuggerStepThrough]
+    [StackTraceHidden]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ThrowIfNullOrEmpty<T>(
+        [NotNull] IEnumerable<T> argument,
+        [CallerArgumentExpression(nameof(argument))] string? paramName = null
+    )
+    {
+        if (argument is null)
+        {
+            ThrowArgumentNullException(paramName);
+        }
+
+        if (argument.TryGetNonEnumeratedCount(out var count) && count == 0)
+        {
+            ThrowArgumentException(paramName);
+        }
     }
 }
